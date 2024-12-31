4e98a71c8e16971428eccb7bd63fbcf03fb0a7ff
Aayu Parikh <aryanparikh03@gmail.com>
2024-12-31 12:53:33 +0530

    Basic demo related to JPA and EntityManager is implemented

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.example.entities.Student;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("JpaDemo");
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+
+            Student s1 = new Student();
+            s1.setId(2);
+            s1.setName("Aryan");
+            em.persist(s1);
+
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+            emf.close();
+        }
     }
 }
