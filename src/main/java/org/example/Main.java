7d9c83ca5ab92e1941e274ac725f13fcbbaa5a9b
Aayu Parikh <aryanparikh03@gmail.com>
2024-12-31 13:18:54 +0530

    Make implementation related to programmatic approach for EntityManager

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,17 +4,23 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import org.example.entities.Student;
+import org.example.persistence.CustomPersistenceUnitInfo;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("JpaDemo");
+        EntityManagerFactory emf = new HibernatePersistenceProvider()
+                .createContainerEntityManagerFactory(new CustomPersistenceUnitInfo(),
+                        new HashMap<>());
         EntityManager em = emf.createEntityManager();
         try {
             em.getTransaction().begin();
 
             Student s1 = new Student();
-            s1.setId(2);
-            s1.setName("Aryan");
+            s1.setId(3);
+            s1.setName("ABC");
             em.persist(s1);
 
             em.getTransaction().commit();
